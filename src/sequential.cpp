ec395dc57dd28dfdba3d670bb58202d7fb1b5f6b
apuccia <alessandropuccia97@gmail.com>
2022-06-25 14:03:04 +0200

    better statistics

--- src/sequential.cpp
+++ src/sequential.cpp
@@ -50,47 +50,80 @@ int main(int argc, char **argv)
 
     VideoMotionDetection detector =
         VideoMotionDetection(path, k_size, thresh);
+    if (stats)
+        detector.print_info();
     Utimer timer = Utimer();
-    if (stats) detector.print_info();
 
+    int t_frames = detector.get_num_frames() - 1;
     int detected = 0;
-    long t_read = 0, t_padding = 0, t_grey = 0, t_convolute = 0, t_detect = 0;
-    while (1)
+
+    // total times in 10 iterations
+    long t_read = 0, t_padding = 0, t_grey = 0, t_convolute = 0, t_detect = 0, t_total = 0;
+    for (int i = 0; i < 10; i++)
     {
-        Mat frame;
+        // total times per iteration
+        long it_read = 0, it_padding = 0, it_grey = 0, it_convolute = 0, it_detect = 0;
+        while (1)
+        {
+            timer.start();
+            Mat frame;
+            detector.next_frame(frame);
+            if (frame.empty())
+                break;
+            it_read += timer.stop();
 
-        timer.start();
-        frame = detector.next_frame();
-        if (frame.empty())
-            break;
-        t_read += timer.stop();
+            timer.start();
+            Mat f_padded;
+            detector.pad_frame(frame, f_padded);
+            it_padding += timer.stop();
 
-        timer.start();
-        frame = detector.pad_frame(frame);
-        t_padding += timer.stop();
+            timer.start();
+            Mat f_grey = Mat::zeros(f_padded.rows, f_padded.cols, CV_8UC1);
+            detector.to_greyscale(f_padded, f_grey);
+            it_grey += timer.stop();
 
-        timer.start();
-        frame = detector.to_greyscale(frame);
-        t_grey += timer.stop();
+            timer.start();
+            detector.convolve(f_grey);
+            it_convolute += timer.stop();
 
-        timer.start();
-        detector.convolve(frame);
-        t_convolute += timer.stop();
+            timer.start();
+            detected += detector.detect(f_grey);
+            it_detect += timer.stop();
+        }
 
-        timer.start();
-        detected += detector.detect(frame);
-        t_detect += timer.stop();
+        t_read += it_read;
+        t_padding += it_padding;
+        t_grey += it_grey;
+        t_convolute += it_convolute;
+        t_detect += it_detect;
+
+        // reset to first frame (background excluded)
+        detector.reset_video();
     }
+    
+    t_total += t_read + t_padding + t_grey + t_convolute + t_detect;
 
-    int t_frames = detector.get_num_frames();
-    std::cout << "---------- RESULTS ----------" << std::endl;
-    std::cout << "Detected " << detected << " frames" << std::endl;
-    std::cout << "Average time spent on reading: " << t_read / t_frames << std::endl;
-    std::cout << "Average time spent on padding: " << t_padding / t_frames << std::endl;
-    std::cout << "Average time spent on greyscaling: " << t_grey / t_frames << std::endl;
-    std::cout << "Average time spent on convoluting: " << t_convolute / t_frames << std::endl;
-    std::cout << "Average time spent on detecting: " << t_detect / t_frames << std::endl;
-    std::cout << "-----------------------------" << std::endl;
+    std::cout << "---------- RESULTS: average on 10 iterations ----------" << std::endl;
+    std::cout << "Total frames: " << t_frames << std::endl;
+    std::cout << "Detected " << detected / 10 << " frames" << std::endl;
+    std::cout << "***** Read" << std::endl;
+    std::cout << "Average time spent on reading a frame: " << (t_read / t_frames) / 10 << std::endl;
+    std::cout << "Total time spent on reading frames: " << t_read / 10 << std::endl;
+    std::cout << "***** Pad" << std::endl;
+    std::cout << "Average time spent on padding a frame: " << (t_padding / t_frames) / 10 << std::endl;
+    std::cout << "Total time spent on padding a frame: " << t_padding / 10 << std::endl;
+    std::cout << "***** Greyscale" << std::endl;
+    std::cout << "Average time spent on greyscaling a frame: " << (t_grey / t_frames) / 10 << std::endl;
+    std::cout << "Total time spent on greyscaling a frame: " << t_grey / 10 << std::endl;
+    std::cout << "***** Convolution" << std::endl;
+    std::cout << "Average time spent on convoluting a frame: " << (t_convolute / t_frames) / 10 << std::endl;
+    std::cout << "Total time spent on convoluting a frame: " << t_convolute / 10 << std::endl;
+    std::cout << "***** Detection" << std::endl;
+    std::cout << "Average time spent on detecting a frame: " << (t_detect / t_frames) / 10 << std::endl;
+    std::cout << "Total time spent on detecting a frame: " << t_detect / 10 << std::endl;
+    std::cout << "***** Total" << std::endl;
+    std::cout << "Total time spent to process the whole stream: " << t_total / 10 << std::endl;
+    std::cout << "-------------------------------------------------------" << std::endl;
 
     return 0;
 }
